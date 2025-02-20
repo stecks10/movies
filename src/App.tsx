6d29e897a84ec6473a226a272363dcd2a0cace80
vitor <stecks10@gmail.com>
2025-02-20 10:49:17 -0300

    adiciona componente Header e separador; remove modo de tema "system" do ModeToggle; atualiza dependências no package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Header } from "./components/header";
 import { ThemeProvider } from "./components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle />
+      <Header />
     </ThemeProvider>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import logoLight from "../assets/logo-cubos-light.svg";
+import logoDark from "../assets/logo-cubos-dark.svg";
+import { useTheme } from "./theme-provider";
+import { ModeToggle } from "./mode-toggle";
+import { Separator } from "@/components/ui/separator";
+
+export function Header() {
+  const { theme } = useTheme();
+  return (
+    <>
+      <header className="flex justify-between items-center p-6">
+        <img
+          src={theme === "dark" ? logoDark : logoLight}
+          alt="Logo Cubos"
+          className="h-8"
+        />
+        <h1 className="text-start flex-1 ml-3 text-black dark:text-white text-bold text-2xl">
+          Movies
+        </h1>
+        <ModeToggle />
+      </header>
+      <Separator />
+    </>
+  );
+}
