d4fc2158b5b184fa7ff4e55d88525795ee2cb84f
vitor <stecks10@gmail.com>
2025-02-20 10:29:21 -0300

    adiciona suporte a temas com ThemeProvider e ModeToggle; implementa utilitário de classe cn; atualiza configurações do TypeScript e Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+    </ThemeProvider>
   );
 }
 
