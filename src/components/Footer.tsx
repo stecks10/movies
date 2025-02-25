91275a352f160819bd45d3864457bca45521c619
vitor <stecks10@gmail.com>
2025-02-25 12:27:35 -0300

    ajusta o padding superior no componente Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="dark:bg-black dark:text-white py-4 md:fixed md:bottom-0 md:w-full border-t">
+    <footer className="dark:bg-black dark:text-white py-4 md:fixed md:bottom-0 md:w-full border-t pt-8">
       <div className="container mx-auto text-center flex justify-center">
         <h1 className="text-sm">
           2025 © Todos os direitos reservados a Cubos Movies
