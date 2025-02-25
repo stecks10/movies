04b1b52bb1364e52d12f3a3f21f8cc3e71e0687e
vitor <stecks10@gmail.com>
2025-02-25 10:18:09 -0300

    Ajuste responsividade no details

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -7,7 +7,7 @@ const StatCard = ({
   title,
   children,
 }: {
-  title: string;
+  title?: string;
   children: React.ReactNode;
 }) => {
   const value = String(children);
@@ -15,18 +15,20 @@ const StatCard = ({
 
   return (
     <div
-      className="bg-zinc-500 text-white rounded-lg p-4 shadow-lg w-full"
+      className="bg-zinc-500 text-white rounded-lg p-2 sm:p-4 shadow-lg w-full"
       style={{
         backgroundColor: "rgba(30, 29, 27, 0.8)",
         boxShadow: "0 4px 14px rgba(0, 0, 0, 0.25)",
       }}
     >
-      <p className="font-semibold uppercase text-sm text-gray-400 text-center overflow-hidden text-ellipsis">
+      <p className="font-semibold uppercase text-xs sm:text-sm text-gray-400 text-center overflow-hidden text-ellipsis">
         {title}
       </p>
       <p
         className={`mt-1 text-center whitespace-nowrap overflow-hidden text-ellipsis ${
-          isLongValue ? "text-base" : "text-xl"
+          isLongValue ? "text-sm" : "text-base"
+        } sm:${isLongValue ? "text-base" : "text-xl"} ${
+          isLongValue ? "leading-tight" : "leading-normal"
         }`}
       >
         {children}
@@ -82,6 +84,33 @@ export function MovieDetails() {
             {movie.title || "Título não disponível"}
           </h1>
 
+          <div className="lg:hidden grid grid-cols-3 gap-2">
+            <StatCard title="Popularidade">
+              {movie.popularity || "Não disponível"}
+            </StatCard>
+            <StatCard title="Votos">
+              {movie.vote_count || "Não disponível"}
+            </StatCard>
+            <div className="flex justify-center items-center">
+              <div
+                className="w-16 h-16 sm:w-20 sm:h-20 rounded-full bg-yellow-300 shadow-2xl flex items-center justify-center"
+                style={{
+                  background: `conic-gradient(
+                    yellow ${Math.round(movie.rating * 10)}%, 
+                    transparent ${Math.round(movie.rating * 10)}% 100%
+                  )`,
+                  boxShadow: "0 0 10px 5px rgba(255, 223, 0, 0.5)",
+                }}
+              >
+                <div className="flex items-center justify-center w-12 h-12 sm:w-16 sm:h-16 bg-black rounded-full">
+                  <span className="text-white font-bold text-xs sm:text-sm">
+                    {Math.round(movie.rating * 10)}%
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div
             className="bg-zinc-500 text-white rounded-lg p-4 shadow-lg w-full"
             style={{
@@ -89,21 +118,54 @@ export function MovieDetails() {
               boxShadow: "0 4px 14px rgba(0, 0, 0, 0.25)",
             }}
           >
+            <h1 className="text-bold">Sinopse</h1>
             <p>{movie.overview || "Sinopse não disponível"}</p>
           </div>
 
-          <div className="flex flex-wrap gap-2">
-            {(movie.genres ?? []).length > 0 ? (
-              (movie.genres ?? []).map((genre) => (
-                <GenreTag key={genre} genre={genre} />
-              ))
-            ) : (
-              <span className="text-gray-400">Gêneros não disponíveis</span>
-            )}
+          <StatCard>
+            <div className="flex flex-wrap gap-2">
+              {(movie.genres ?? []).length > 0 ? (
+                (movie.genres ?? []).map((genre) => (
+                  <GenreTag key={genre} genre={genre} />
+                ))
+              ) : (
+                <span className="text-gray-400">Gêneros não disponíveis</span>
+              )}
+            </div>
+          </StatCard>
+
+          <div className="lg:hidden grid grid-cols-2 gap-4">
+            <StatCard title="Lançamento">
+              {formatDate(movie.release_date) || "Data não disponível"}
+            </StatCard>
+            <StatCard title="Duração">
+              {movie.runtime ? formatRuntime(movie.runtime) : "Não disponível"}
+            </StatCard>
+            <StatCard title="Situação">
+              {(movie.revenue ?? 0) > 0 ? "Lançado" : "Em cartaz"}
+            </StatCard>
+            <StatCard title="Idioma">
+              {movie.original_language
+                ? movie.original_language.toUpperCase()
+                : "Não informado"}
+            </StatCard>
+          </div>
+          <div className="lg:hidden grid grid-cols-3 gap-4">
+            <StatCard title="Orçamento">
+              {movie.budget ? formatCurrency(movie.budget) : "Não informado"}
+            </StatCard>
+            <StatCard title="Receita">
+              {movie.revenue ? formatCurrency(movie.revenue) : "Não informado"}
+            </StatCard>
+            <StatCard title="Lucro">
+              {movie.revenue && movie.budget
+                ? formatCurrency(movie.revenue - movie.budget)
+                : "Não informado"}
+            </StatCard>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="hidden lg:grid grid-cols-1 lg:grid-cols-3 gap-4">
           <StatCard title="Popularidade">
             {movie.popularity || "Não disponível"}
           </StatCard>
@@ -113,17 +175,17 @@ export function MovieDetails() {
 
           <div className="flex justify-center items-center">
             <div
-              className="w-20 h-20 rounded-full bg-yellow-300 shadow-2xl flex items-center justify-center"
+              className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-yellow-300 shadow-2xl flex items-center justify-center"
               style={{
                 background: `conic-gradient(
-            yellow ${Math.round(movie.rating * 10)}%, 
-            transparent ${Math.round(movie.rating * 10)}% 100%
-            )`,
+                  yellow ${Math.round(movie.rating * 10)}%, 
+                  transparent ${Math.round(movie.rating * 10)}% 100%
+                )`,
                 boxShadow: "0 0 10px 5px rgba(255, 223, 0, 0.5)",
               }}
             >
-              <div className="flex items-center justify-center w-16 h-16 bg-black rounded-full">
-                <span className="text-white font-bold">
+              <div className="flex items-center justify-center w-20 h-20 bg-black rounded-full">
+                <span className="text-white font-bold text-lg sm:text-xl">
                   {Math.round(movie.rating * 10)}%
                 </span>
               </div>
@@ -140,7 +202,7 @@ export function MovieDetails() {
               <StatCard title="Duração">
                 {movie.runtime
                   ? formatRuntime(movie.runtime)
-                  : "Duração não disponível"}
+                  : "Não disponível"}
               </StatCard>
             </div>
           </div>
@@ -155,7 +217,7 @@ export function MovieDetails() {
               <StatCard title="Idioma">
                 {movie.original_language
                   ? movie.original_language.toUpperCase()
-                  : "Nao informado"}
+                  : "Não informado"}
               </StatCard>
             </div>
           </div>
