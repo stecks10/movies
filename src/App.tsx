af35aa48fbeef908eb7cf6aa54ba6e5d10e5bf08
vitor <stecks10@gmail.com>
2025-02-25 04:04:31 -0300

    corrige importação do componente Header para seguir convenções de nomenclatura

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@/hooks/theme-provider";
-import { Header } from "./components/Header";
 import { HomePage } from "./pages/HomePage";
 import { MovieDetails } from "./pages/MovieDetails";
+import { Header } from "./components/header";
 
 function App() {
   return (
